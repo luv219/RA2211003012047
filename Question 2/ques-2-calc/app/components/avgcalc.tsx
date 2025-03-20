ded86005b38586b98204defd15efaeac7828ec5a
Luv Gupta <154324377+luv219@users.noreply.github.com>
2025-03-20 19:24:43 +0530

    Update avgcalc.tsx

--- Question 2/ques-2-calc/app/components/avgcalc.tsx
+++ Question 2/ques-2-calc/app/components/avgcalc.tsx
@@ -20,33 +20,44 @@ export default function AvgCalculator() {
 
   const fetchNumbers = async () => {
     setLoading(true);
+    const endpoint = API_ENDPOINTS[selectedType as keyof typeof API_ENDPOINTS];
 
-    const endpoint =API_ENDPOINTS[selectedType as keyof typeof API_ENDPOINTS];
-    if(!endpoint){
-        console.error("Invalid endpoint:", selectedType);
-        setLoading(false);
-        return;
+    if (!endpoint) {
+      console.error("Invalid endpoint:", selectedType);
+      setLoading(false);
+      return;
     }
 
     try {
-      const response = await fetch(endpoint);
+      const response = await fetch(endpoint, {
+        method: "GET",
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiZXhwIjoxNzQyNDc3NjEyLCJpYXQiOjE3NDI0NzczMTIsImlzcyI6IkFmZm9yZG1lZCIsImp0aSI6ImVmYTMxZGFhLTNhNTAtNGFiYS1iYTRkLWM4MjE1YTg3MWMyZSIsInN1YiI6InNnOTg5MUBzcm1pc3QuZWR1LmluIn0sImNvbXBhbnlOYW1lIjoiU1JNSVNUIiwiY2xpZW50SUQiOiJlZmEzMWRhYS0zYTUwLTRhYmEtYmE0ZC1jODIxNWE4NzFjMmUiLCJjbGllbnRTZWNyZXQiOiJETklsT3VWZlpaTXNIZnVpIiwib3duZXJOYW1lIjoiU2FoaWwgR3VwdGEiLCJvd25lckVtYWlsIjoic2c5ODkxQHNybWlzdC5lZHUuaW4iLCJyb2xsTm8iOiJSQTIyMTEwMDMwMTE4MDkifQ.FzOMrApDt6R8OJvgTK9-Sbw-844oCbe4to1BADQUbvw",
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (!response.ok)
+        throw new Error(`HTTP error! Status: ${response.status}`);
+
       const data = await response.json();
       const numbers = data.numbers.slice(0, WINDOW_SIZE);
 
       setWindowPrevState([...windowCurrState]);
       setWindowCurrState(numbers);
-
-      const avgCalc = numbers.reduce((a, b) => a + b, 0) / numbers.length;
-      setAvg(avgCalc);
+      setAvg(numbers.reduce((a, b) => a + b, 0) / numbers.length);
     } catch (error) {
       console.error("Error fetching numbers:", error);
+      alert("Failed to fetch numbers.");
+    } finally {
+      setLoading(false);
     }
-    setLoading(false);
   };
 
   useEffect(() => {
-    const fetchData = async() => {
-        await fetchNumbers();
+    const fetchData = async () => {
+      await fetchNumbers();
     };
     fetchData();
   }, [selectedType]);
@@ -61,10 +72,18 @@ export default function AvgCalculator() {
         onChange={(e) => setSelectedType(e.target.value)}
         className="border p-2 mb-4 w-full"
       >
-        <option key="p" value="p">Prime Numbers</option>
-        <option key="f" value="f">Fibonacci Numbers</option>
-        <option key="e" value="e">Even Numbers</option>
-        <option key="r" value="r">Random Numbers</option>
+        <option key="p" value="p">
+          Prime Numbers
+        </option>
+        <option key="f" value="f">
+          Fibonacci Numbers
+        </option>
+        <option key="e" value="e">
+          Even Numbers
+        </option>
+        <option key="r" value="r">
+          Random Numbers
+        </option>
       </select>
 
       <button
